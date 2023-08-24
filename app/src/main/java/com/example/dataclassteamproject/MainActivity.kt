02d80a3227a3e4439215947f79fa77079fe6f21a
이홍진\iit80 <hong80714@gmail.com>
2023-08-24 16:05:48 +0900

    Project start 프로젝트의 기초틀 Home, Dm, Schedule, PersonalInfo의 화면으로 구성 이제 디자인과 기능구현을 하면 된다.

--- app/src/main/java/com/example/dataclassteamproject/MainActivity.kt
+++ app/src/main/java/com/example/dataclassteamproject/MainActivity.kt
@@ -3,13 +3,37 @@ package com.example.dataclassteamproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.dataclassteamproject.ui.theme.DataClassTeamProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +41,195 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DataClassTeamProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") {
+                        HomeScreen(navController)
+                    }
+                    composable("dm") {
+                        DmScreen(navController)
+                    }
+                    composable("schedule") {
+                        ScheduleScreen(navController)
+                    }
+                    composable("personal") {
+                        PersonalInfoScreen(navController)
+                    }
+                    composable("boardview") {
+                        //여기에 보드뷰 스크린을 넣어주세요
+                    }
+                    //추가해야할 스크린
+                    //채팅방
                 }
+            }
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            MyTopBar("home")
+        },
+        bottomBar = {
+            MyBottomBara(navController)
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Column {
+                Text(text = "게시판")
+                Text(text = "게시판")
+                Text(text = "게시판")
+                Text(text = "게시판")
+                Text(text = "게시판")
+                Text(text = "게시판")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DmScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            MyTopBar("Dm")
+        },
+        bottomBar = {
+            MyBottomBara(navController)
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Column {
+                repeat(10) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .border(1.dp, Color.Black, RectangleShape)
+                    ) {
+                        Text(text = "DM")
+                    }
+                }
+            }
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScheduleScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            MyTopBar("스케줄")
+        },
+        bottomBar = {
+            MyBottomBara(navController)
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Text(text = " 대충 달력")
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PersonalInfoScreen(navController: NavController) {
+
+    Scaffold(
+        topBar = {
+            MyTopBar("개인정보")
+        },
+        bottomBar = {
+            MyBottomBara(navController)
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "profile",
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                )
+                Text(text = "이름")
+                Text(text = "정보")
+                Text(text = "정보")
+                Text(text = "정보")
+                Text(text = "정보")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+@OptIn(ExperimentalMaterial3Api::class)
+private fun MyTopBar(topBarTitle: String) {
+    TopAppBar(
+        title = { Text(text = topBarTitle) },
+        //탑바 색바꾸기
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Gray)
     )
 }
+@Composable
+private fun MyBottomBara(navController: NavController) {
+    BottomAppBar(
+        containerColor = Color.Gray
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(onClick = {
+                navController.navigate("home")
+            }) {
+                Text(text = "home")
+            }
+            Button(onClick = {
+                navController.navigate("dm")
+            }) {
+                Text(text = "DM")
+            }
+            Button(onClick = {
+                navController.navigate("schedule")
+            }) {
+                Text(text = "스케쥴")
+            }
+            Button(onClick = {
+                navController.navigate("personal")
+            }) {
+                Text(text = "개인정보")
+            }
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    DataClassTeamProjectTheme {
-        Greeting("Android")
-    }
+
 }
