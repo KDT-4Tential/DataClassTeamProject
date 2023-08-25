83e301bc94debc07ac89de6e99add77caa914b99
COMPUTER\asa11 <asa1186@naver.com>
2023-08-25 11:00:34 +0900

    달력추가

--- app/src/main/java/com/example/dataclassteamproject/MainActivity.kt
+++ app/src/main/java/com/example/dataclassteamproject/MainActivity.kt
@@ -4,37 +4,64 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.dataclassteamproject.ui.theme.DataClassTeamProjectTheme
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,7 +79,10 @@ class MainActivity : ComponentActivity() {
                         DmScreen(navController)
                     }
                     composable("schedule") {
-                        ScheduleScreen(navController)
+                        ScheduleScreen( navController = rememberNavController(),
+                            onPreviousMonthClick = {},
+                            onNextMonthClick = {}
+                        )
                     }
                     composable("personal") {
                         PersonalInfoScreen(navController)
@@ -67,6 +97,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -125,9 +156,14 @@ fun DmScreen(navController: NavController) {
         }
     }
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScheduleScreen(navController: NavController) {
+fun ScheduleScreen(navController: NavController,
+                   onPreviousMonthClick: () -> Unit,
+                   onNextMonthClick: () -> Unit
+
+) {
     Scaffold(
         topBar = {
             MyTopBar("스케줄")
@@ -136,16 +172,173 @@ fun ScheduleScreen(navController: NavController) {
             MyBottomBara(navController)
         }
     ) { innerPadding ->
+
+        var selectedDate by remember { mutableStateOf(LocalDate.now()) }
+
         Column(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
         ) {
-            Text(text = " 대충 달력")
-
+            CalendarComposable(
+                modifier = Modifier.fillMaxWidth(),
+                selectedDate = selectedDate,
+                onDateSelected = { selectedDate, position -> },
+                onPreviousMonthClick = {
+                    selectedDate = selectedDate.minusMonths(1)
+                    onPreviousMonthClick()
+                },
+                onNextMonthClick = {
+                    selectedDate = selectedDate.plusMonths(1)
+                    onNextMonthClick()
+                }
+            )
         }
     }
 }
+
+// 달력구성 컴포저블
+            @Composable
+            fun CalendarComposable(
+                modifier: Modifier = Modifier,
+                selectedDate: LocalDate,
+                onDateSelected: (LocalDate, Int) -> Unit,
+                onPreviousMonthClick: () -> Unit,
+                onNextMonthClick: () -> Unit
+            ) {
+                Column(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        IconButton(onClick = onPreviousMonthClick) {
+                            Icon(Icons.Default.ArrowBack, contentDescription = "Previous Month")
+                        }
+
+                        val headerText =
+                            selectedDate.format(DateTimeFormatter.ofPattern("yyyy년 M월"))
+                        Text(
+                            text = headerText,
+                            style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 18.sp),
+                        )
+
+                        IconButton(onClick = onNextMonthClick) {
+                            Icon(Icons.Default.ArrowForward, contentDescription = "Next Month")
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "일",
+                            modifier = Modifier.weight(1f),
+                            color = Color.Red,
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = "월",
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = "화",
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = "수",
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = "목",
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = "금",
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = "토",
+                            modifier = Modifier.weight(1f),
+                            color = Color.Blue,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                    val firstDayOfMonth = selectedDate.withDayOfMonth(1)
+                    val lastDayOfMonth = selectedDate.withDayOfMonth(selectedDate.lengthOfMonth())
+
+                    val daysInMonth = (1..lastDayOfMonth.dayOfMonth).toList()
+                    val emptyDaysBefore = (1 until firstDayOfMonth.dayOfWeek.value).toList()
+
+
+                    LazyVerticalGrid(
+                        GridCells.Fixed(7), // 각 행당 7개의 열을 가지도록 설정
+                        contentPadding = PaddingValues(4.dp),
+                        verticalArrangement = Arrangement.spacedBy(90.dp),
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        // Display empty boxes for days before the first day of the month
+                        items(emptyDaysBefore) {
+                            Spacer(modifier = Modifier.size(30.dp))
+                        }
+
+                        itemsIndexed(daysInMonth) { index, day ->
+                            val date = selectedDate.withDayOfMonth(day)
+                            val isSelected = date == selectedDate
+                            Divider()
+                            CalendarDay(
+                                date = date,
+                                isSelected = isSelected,
+                                onDateSelected = { onDateSelected }
+                            )
+                        }
+                    }
+                }
+            }
+
+
+
+@Composable
+fun CalendarDay(
+    date: LocalDate,
+    isSelected: Boolean,
+    onDateSelected: (LocalDate) -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .size(30.dp)
+            .clip(CircleShape)
+            .clickable { onDateSelected(date) }
+            .background(if (isSelected) Color.Gray else Color.Transparent),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = date.dayOfMonth.toString(),
+            fontWeight = FontWeight.Bold,
+            color = if (isSelected) Color.White else Color.Black
+        )
+//        if (hasExerciseRecord) { // 운동 기록이 있는 경우 점을 추가로 표시 (예시로남겨둠)
+//            Box(
+//                modifier = Modifier
+//                    .size(6.dp)
+//                    .background(Color.Green, CircleShape)
+//                    .align(Alignment.BottomCenter)
+//            )
+//        }
+    }
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -193,6 +386,7 @@ private fun MyTopBar(topBarTitle: String) {
         colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Gray)
     )
 }
+
 @Composable
 private fun MyBottomBara(navController: NavController) {
     BottomAppBar(
@@ -230,6 +424,11 @@ private fun MyBottomBara(navController: NavController) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-
+fun DefaultPreview() {
+    DataClassTeamProjectTheme {
+        ScheduleScreen( navController = rememberNavController(),
+            onPreviousMonthClick = {},
+            onNextMonthClick = {}
+        )
+    }
 }
